c470f3502edefcfb6ce0a409c518c463fe6d4732
caio17ffn <flaviocaio40@gmail.com>
2026-02-26 08:32:05 -0300

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,5 @@
+resp = input("Vôce vai passar de ano? s/n: ")
+resultado = bool(resp)
+
+print("Resposta", resp)
+print("Resultado", resultado)
---
+++ lower.py
@@ -0,0 +1,6 @@
+resp = input("Vôce vai passar de ano? s/n: "). strip().lower()
+
+resultado = (resp == "s")
+
+print("Resultado", resultado)
+print(type(resultado))
